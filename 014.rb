7519bd0cf31c46a4b4ea60c27b1e20387ace97ed
Markus Prinz <markus.prinz@nuclearsquid.com>
2009-01-14 00:32:59 +0100

    A few more improvements to Problem 14

--- 014.rb
+++ 014.rb
@@ -5,18 +5,17 @@ $cache = {1 => 1}
 
 def chain(n)
   $cache[n] ||= 1 + chain((n & 0x1) == 0 ? n / 2 : 3*n + 1)
-  $cache[n]
 end
 
-$best_number = 0
-$best_length = 0
+best_number = 0
+best_length = 0
 
 1_000_000.downto(1) do |num|
   length = chain(num)
-  if length > $best_length
-    $best_number = num 
-    $best_length = length
+  if length > best_length
+    best_number = num
+    best_length = length
   end
 end
 
-puts "#{$best_number} with l=#{$best_length}"
+puts "#{best_number} with l=#{best_length}"
