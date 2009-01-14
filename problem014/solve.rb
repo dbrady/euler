98e454c18abeaff42017e944d5352f4adfc046b6
David Brady <dbrady@shinybit.com>
2009-01-13 20:42:34 -0700

    Solved problem 20

--- problem014/solve.rb
+++ problem014/solve.rb
@@ -1,15 +1,17 @@
 #!/usr/bin/env ruby1.9
 #!/usr/bin/env jruby
-require '../lib/integer.rb' # for Numeric#even?
 
 # This is quite elegant, but also VERY slow under MRI 1.8. Run under
 # jruby or ruby1.9!
 
-# See solve2.rb for a fast Ruby 1.8 solution:
+# See solve2.rb for a fast Ruby 1.8 solution. Here's why this is slow:
+# 
 # - Initting a hash with a proc is clever but slow. 100s -> 16s after
 #   writing code to cache the results by hand.
+# 
 # - Looping with downto instead of using a map prevents duplication of
 #   the array. This results in another 16s -> 4s speed impromevent.
+# 
 # - Interestingly, I noticed that h might be aliasing to the external
 #   h variable, so I changed the inner variables to |hash,key|. This
 #   added 20sec to the execution time. Curious.
