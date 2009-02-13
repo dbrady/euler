cf3ac3e9d858ba806102a5449ca23ec7ec169e94
David Brady <dbrady@shinybit.com>
2009-02-13 11:47:24 -0700

    Fixed broken problem001/dbrady.rb

--- problem001/dbrady.rb
+++ problem001/dbrady.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+require '../lib/enumerable'
 
 # ----------------------------------------------------------------------
 # If we list all the natural numbers below 10 that are multiples of 3
@@ -7,5 +8,10 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 # ----------------------------------------------------------------------
 
-
+# ----------------------------------------------------------------------
+# ruby   : 0.088s
+# ruby1.9: 0.025s
+# jruby  : 0.616s
+# rbx    : 0.440s
+# tinyrb : -.---s (DNF)
 puts (1..999).to_a.delete_if { |i| !((i%3).zero? || (i%5).zero?) }.sum
